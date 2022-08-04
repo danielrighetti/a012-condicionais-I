3d37cc112bb828dd0643a890138234c0404ff452
Daniel Righetti <danrighetti27@gmail.com>
2022-08-04 14:25:40 -0300

    Exercícios Resolvidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,13 @@
-const num = //seu numero aqui
+// const num = //seu numero aqui
 
-if(){
-    console.log("sua mensagem aqui");
+// if(){
+//     console.log("sua mensagem aqui");
+// }
+
+const num = prompt('Insira um numero: ') 
+
+if(num % 2 === 0){
+    console.log('O número é par')
+}else if(num % 2 === 1){
+    console.log('O número é ímpar')
 }
